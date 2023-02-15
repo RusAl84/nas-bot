3fde5a6f5035923916dbb38ea098122fdf753edd
Aleksey <rusal@bk.ru>
2023-02-15 17:42:21 +0300

    first commit

---
+++ getKeywords.py
@@ -0,0 +1,52 @@
+﻿import nltk
+import pymorphy2
+import string
+
+def remove_digit(str1):
+    str2 = ''
+    for c in str1:
+        if c not in ('0', "1", '2', '3', '4', '5', '6', '7', '8', '9', '«', '»', '–', "\""):
+            str2 = str2 + c
+    return str2
+
+def remove_punctuation(str1):
+    str2 = ''
+    pattern = string.punctuation
+    for c in str1:
+        if c not in pattern:
+            str2 = str2 + c
+        else:
+            str2 = str2 + ""
+    return str2
+    
+def getKeywords(str1):
+    mas=[]
+    morph = pymorphy2.MorphAnalyzer()
+    for i in str1.split():
+        if len(i)>2:
+            p = morph.parse(i)[0]
+            str2=p.normal_form
+            str2 =remove_digit(str2)
+            str2 =remove_punctuation(str2)
+            mas.append(str2)
+    set2 = set()
+    for item in mas:
+        if not "nan" in str(item).replace(" nan ", " "):
+            set2.add(str(item).replace(" nan ", " "))
+    mas = list(set2)
+    mas.sort()
+    return(mas)
+
+if __name__ == '__main__':
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    str1="Ежи – это небольшие животные с иголками, вес которых редко превышает 1 килограмм."
+    str1="Точная наука, первоначально исследовавшая количественные отношения и пространственные формы."
+    str1="Область естествознания: наука о наиболее общих законах природы, о материи, её структуре, движении и правилах трансформации."
+    mas=getKeywords(str1)
+    str2=""
+    for i in mas:
+        str2+=f", \"{i}\""
+    str2=f"[{str2[2:]}]"
+    print(str2)
+    
---
+++ tester.py
@@ -0,0 +1,35 @@
+﻿import json
+import getKeywords as gk
+
+
+def load_data():
+    content = ""
+    with open('data.json', 'r', encoding='utf-8') as f:
+        content = f.read()
+    # print(content)
+    test_data = json.loads(content)
+    return test_data
+
+
+def get_question(test_data, num):
+    str1 = test_data[num]['question']+"\n"
+    str1 += "Введите ответ в развернутой форме:"
+    return str1
+    
+def check_answer(test_data, num, answer_text):
+    result = 0
+    if num < len(test_data):
+        countIntersections=0
+        keywords1=gk.getKeywords(answer_text)
+        keywords2=test_data[num]['keyWords']
+        for i1 in keywords1:
+            for i2 in keywords2:
+                if i1==i2:
+                    countIntersections+=1
+        result = countIntersections / len(keywords1) * 100
+    return result
+
+
+if __name__ == '__main__':
+    test_data = load_data()
+    quest = test_data[0]
